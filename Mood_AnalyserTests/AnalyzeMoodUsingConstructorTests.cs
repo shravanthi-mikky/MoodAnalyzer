94f1a151c9460f025abe5306817f65ef05b879f8
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-18 12:27:43 +0530

    [shravanthi] - Tc for refactor1

---
+++ Mood_AnalyserTests/AnalyzeMoodUsingConstructorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser.Tests
+{
+    [TestClass()]
+    public class AnalyzeMoodUsingConstructorTests
+    {
+        [TestMethod()]
+        public void AnalyseMoodMethod1Test()
+        {
+            AnalyzeMoodUsingConstructor a1 = new();
+            string mood1 = a1.AnalyseMoodMethod1();
+            Assert.AreEqual("SAD", mood1);
+        }
+    }
+}
