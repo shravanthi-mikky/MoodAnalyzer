54897d8c893a2a8043036d952a7ba246356eed61
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-18 17:43:09 +0530

    [shravanthi] - Exception Handling

---
+++ Mood_Analyser/MoodAnalyzerException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyzerException
+    {
+        public string MoodAnalyzerExceptionMethod(string message)
+        {
+            try
+            {
+                string message1 = "SAD";
+                string message2 = "HAPPY";
+                if (message.ToUpper().Contains(message1.ToUpper()))
+                {
+                    return message1;
+                }
+                else if(message.ToUpper().Contains(message2.ToUpper()))
+                {
+                    return message2;
+                }
+                
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("You have provided null reference!");
+                Console.WriteLine(ex.Message);
+                
+            }
+            return null;
+        }
+    }
+}
--- Mood_Analyser/Program.cs
+++ Mood_Analyser/Program.cs
@@ -2,14 +2,33 @@
 using Mood_Analyser;
 
 Console.WriteLine("Mood Analyser!");
-AnalyseMood a0 = new();
-string mood0 = a0.AnalyseMoodMethod("I am in sad mood");
-Console.WriteLine(mood0);
 
-AnalyzeMoodUsingConstructor a1 = new();
-string mood1 = a1.AnalyseMoodMethod1();
-Console.WriteLine("Using the Default Constructor here and mood is:"+mood1);
+while (true)
+{
+    Console.WriteLine("Please choose the option :\n1)Basic Mood Analyzer\n2)Using the Default\\Parameterized Constructor\n3)Exception");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            AnalyseMood a0 = new();
+            string mood0 = a0.AnalyseMoodMethod("I am in sad mood");
+            Console.WriteLine(mood0);
+            break;
+        case 2:
+            AnalyzeMoodUsingConstructor a1 = new();
+            string mood1 = a1.AnalyseMoodMethod1();
+            Console.WriteLine("Using the Default Constructor here and mood is:" + mood1);
 
-AnalyzeMoodUsingConstructor a2 = new("I am in Happy Mood");
-string mood2 = a2.AnalyseMoodMethod1();
-Console.WriteLine("Using the Parameterized Constructor here and mood is:" + mood2);
+            AnalyzeMoodUsingConstructor a2 = new("I am in Happy Mood");
+            string mood2 = a2.AnalyseMoodMethod1();
+            Console.WriteLine("Using the Parameterized Constructor here and mood is:" + mood2);
+            break;
+        case 3:
+            MoodAnalyzerException a3 = new MoodAnalyzerException();
+            a3.MoodAnalyzerExceptionMethod(null);
+
+            break;
+        default:
+            break;
+    }
+}
